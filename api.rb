2c4fe123982de4f2dce4cacfc210070803b0ad74
Vova Kholodenko <vkhodenko@homeaway.com>
2018-11-01 17:04:24 +0200

    chromedriver

--- api.rb
+++ api.rb
@@ -2,6 +2,10 @@ require 'sinatra'
 require 'watir'
 
 get '/test' do
+  # chromedriver_path = File.join(File.absolute_path('../..', File.dirname(__FILE__)),"chromedriver")
+  Selenium::WebDriver::Chrome.driver_path = "./chromedriver"
+
+
   browser = Watir::Browser.new
 
   browser.goto 'https://www.google.com.ua/'
